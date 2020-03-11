6e3cc63966d0cc061033916b52bd8716092eaac5
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-11 10:49:32 +0900

    make nav toggle btn && dummy top

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const nav_bar_btn = document.querySelector('.toggle-btn')
+const nav_bar__toggle = document.querySelector('#nav-bar__toggle')
+const menu_bar = document.querySelector('#menu-bar')
+
+const showMenuEvent = (el) => {
+    el.addEventListener('click', (e) => {
+        e.stopPropagation();
+        nav_bar__toggle.classList.toggle('active')
+        menu_bar.classList.toggle('active')
+    })
+}
+
+showMenuEvent(nav_bar_btn)
+showMenuEvent(nav_bar__toggle)
