e2fb1d81af32b4a631e7f57956b4c786ce4d933a
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-11 14:34:48 +0900

    make slider && make demo1 page

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const SHOWING_CLASS = 'showing'
+const firstSlide = document.querySelector('.slider .slider__item:first-child')
+
+const slide = () => {
+    const currentSlide = document.querySelector(`.slider .slider__item.${SHOWING_CLASS}`)
+    if (currentSlide) {
+        currentSlide.classList.remove(SHOWING_CLASS)
+        const nextSlide = currentSlide.nextElementSibling
+        if (nextSlide) {
+            nextSlide.classList.add(SHOWING_CLASS)
+        } else {
+            firstSlide.classList.add(SHOWING_CLASS)
+        }
+    } else {
+        firstSlide.classList.add(SHOWING_CLASS)
+    }
+}
+
+slide()
+
+setInterval(slide, 3000)
